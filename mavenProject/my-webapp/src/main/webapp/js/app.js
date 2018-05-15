45aa5fef53e1454212100be2e4b3dc97d1ddce17
1969308600 <1969308600@qq.com>
2018-05-15 09:41:30 +0800

    2018-5-15  解决秒出，静态话菜单

--- mavenProject/my-webapp/src/main/webapp/js/app.js
+++ mavenProject/my-webapp/src/main/webapp/js/app.js
@@ -1,4 +1,4 @@
-define([ 'angularAMD', 'angular-route', 'angular', 'jquery' ], function(
+define([ 'angularAMD', 'angular-route', 'angular', 'jquery','systemConfigs' ], function(
 		angularAMD) {
 	var app = angular.module('app', [ 'ui.router' ]);
 
@@ -8,11 +8,13 @@ define([ 'angularAMD', 'angular-route', 'angular', 'jquery' ], function(
 	});
 
 	app.controller('myAppController', function($scope, $state) {//系统controller，加载资源（菜单，字典。。）
-		$scope.test = "xwQ test!";
-		$scope.menus = [];
-		$scope.thisMenu = 1;//默认系统管理id
+		$scope.test = "xwQ";
+		$scope.currMenuName = "首页";
+		$scope.menus = sysMenus;//设置静态默认值
+		$scope.thisMenu = 11;//默认系统管理id
 		//获取系统  菜单
 		$scope.getSysTemMenu = function() {
+			debugger
 			$.post("/system/getMenu.do", {}, function(res) {//预留角色参数，后期将以角色获取菜单。
 				$scope.menus = JSON.parse(res).data;
 				$scope.$digest();
@@ -21,35 +23,41 @@ define([ 'angularAMD', 'angular-route', 'angular', 'jquery' ], function(
 		}
 		$scope.getSysTemMenu();
 
-		$scope.choiceMenu = function(id) {//父级菜单选择
+		$scope.choiceMenu = function(id,name) {//父级菜单选择
 			$scope.thisMenu = id;
+			$scope.currMenuName =name;
 			//默认跳转子菜单第一个
 		}
 
 		$scope.choiceChildMenu = function(id) {
 
 		}
+		
+	
 	});
+	
+	var loadtime = function (scope,$stateProvider){
+		
+		sysMenus.forEach(function(value) {
+			if (value.urlKey) {
+				$stateProvider.state(value.urlKey, angularAMD.route({
+					url : '/' + value.urlKey,
+					templateUrl : value.urlHtml,
+					controller : value.ctrlKey,
+					controllerUrl : value.urlCtrl,
+				}))
+			}
+		});
+	}
 	//路由配置
 	app.config(function($stateProvider) {
-		
-		setTimeout(function() {//延时加载，因为scope.menus是在appcontroller里获取的，必须要等他先出来再去设置路由，而且不能写在ajax里，还没找到解决方案。
-			
+	 	setTimeout(function() {//延时加载，因为scope.menus是在appcontroller里获取的，必须要等他先出来再去设置路由，而且不能写在ajax里，
+	 		//方案一：菜单静态化
 			var scope = angular.element(
-					$('div[ ng-controller="myAppController"]')).scope();
-			scope.menus.forEach(function(value) {
-				if (value.urlKey) {
-					$stateProvider.state(value.urlKey, angularAMD.route({
-						url : '/' + value.urlKey,
-						templateUrl : value.urlHtml,
-						controller : value.ctrlKey,
-						controllerUrl : value.urlCtrl,
-					}))
-				}
-			});
-			
+					$('html[ng-controller="myAppController"]')).scope();
+			loadtime(scope,$stateProvider)
 			scope.$state.go("menu");
-		}, 100);
+		}, 5); //方案一：菜单静态化     但是scope无法获取，需要延迟！！！！！！！0.005 劲量最优了
 	});
 	return angularAMD.bootstrap(app);
 });
---
+++ mavenProject/my-webapp/src/main/webapp/js/systemConfigs.js
@@ -0,0 +1,202 @@
+/**
+ * 对于菜单不要在数据库里，这样会影响速度， 现在我要的是让用户觉得菜单是秒出的，这样会给人一种秒出的感觉，其他内容稍后些
+ * 
+ * 这儿存在一个问题：
+ * 				菜单静态化了，但是在列表里还是查询的是数据库，如何更新这个sysMenus的问题！
+ * 			这儿将静态化的值作为默认值，查询后重置这个静态化的值是否解决？
+ * 
+ */
+
+var sysMenus = [ {
+	"code" : "code2000",
+	"ctrlKey" : "",
+	"id" : 1,
+	"key" : "",
+	"level" : 1,
+	"name" : "系统管理",
+	"parent" : 0,
+	"urlCtrl" : "",
+	"urlHtml" : "",
+	"urlKey" : "",
+	"$$hashKey" : "object:4"
+}, {
+	"code" : "code2001",
+	"ctrlKey" : "",
+	"id" : 2,
+	"key" : "",
+	"level" : 1,
+	"name" : "你妹",
+	"parent" : 0,
+	"urlCtrl" : "",
+	"urlHtml" : "",
+	"urlKey" : "",
+	"$$hashKey" : "object:5"
+}, {
+	"code" : "code2002",
+	"ctrlKey" : "",
+	"id" : 3,
+	"key" : "",
+	"level" : 1,
+	"name" : "商品管理",
+	"parent" : 0,
+	"urlCtrl" : "",
+	"urlHtml" : "",
+	"urlKey" : "",
+	"$$hashKey" : "object:6"
+}, {
+	"code" : "code2003",
+	"ctrlKey" : "",
+	"id" : 4,
+	"key" : "",
+	"level" : 1,
+	"name" : "其他管理",
+	"parent" : 0,
+	"urlCtrl" : "",
+	"urlHtml" : "",
+	"urlKey" : "",
+	"$$hashKey" : "object:7"
+}, {
+	"code" : "",
+	"ctrlKey" : "",
+	"id" : 5,
+	"key" : "",
+	"level" : 1,
+	"name" : "图形统计",
+	"parent" : 0,
+	"urlCtrl" : "",
+	"urlHtml" : "",
+	"urlKey" : "",
+	"$$hashKey" : "object:8"
+}, {
+	"code" : "",
+	"ctrlKey" : "",
+	"id" : 11,
+	"key" : "",
+	"level" : 1,
+	"name" : "首页（锐理）",
+	"parent" : 0,
+	"urlCtrl" : "",
+	"urlHtml" : "",
+	"urlKey" : "",
+	"$$hashKey" : "object:9"
+}, {
+	"code" : "",
+	"ctrlKey" : "",
+	"id" : 12,
+	"key" : "",
+	"level" : 1,
+	"name" : "新房管理（锐理）",
+	"parent" : 0,
+	"urlCtrl" : "",
+	"urlHtml" : "",
+	"urlKey" : "",
+	"$$hashKey" : "object:10"
+}, {
+	"code" : "code3001",
+	"ctrlKey" : "roleCtrl",
+	"id" : 25,
+	"key" : "/system/role",
+	"level" : 2,
+	"name" : "角色管理",
+	"parent" : 1,
+	"urlCtrl" : "webappJs/sys/roleCtrl",
+	"urlHtml" : "../ftl/sys/role.html",
+	"urlKey" : "role",
+	"$$hashKey" : "object:11"
+}, {
+	"code" : "code3002",
+	"ctrlKey" : "userCtrl",
+	"id" : 26,
+	"key" : "/system/user",
+	"level" : 2,
+	"name" : "用户管理",
+	"parent" : 1,
+	"urlCtrl" : "webappJs/sys/userCtrl",
+	"urlHtml" : "../ftl/sys/user.html",
+	"urlKey" : "user",
+	"$$hashKey" : "object:12"
+}, {
+	"code" : "code3003",
+	"ctrlKey" : "powerCtrl",
+	"id" : 27,
+	"key" : "/system/power",
+	"level" : 2,
+	"name" : "权限管理",
+	"parent" : 1,
+	"urlCtrl" : "webappJs/sys/powerCtrl",
+	"urlHtml" : "../ftl/sys/power.html",
+	"urlKey" : "power",
+	"$$hashKey" : "object:13"
+}, {
+	"code" : "code3004",
+	"ctrlKey" : "menuCtrl",
+	"id" : 28,
+	"key" : "/system/menu",
+	"level" : 2,
+	"name" : "菜单管理",
+	"parent" : 1,
+	"urlCtrl" : "webappJs/sys/menuCtrl",
+	"urlHtml" : "../ftl/sys/menu.html",
+	"urlKey" : "menu",
+	"$$hashKey" : "object:14"
+}, {
+	"code" : "code3005",
+	"ctrlKey" : "flewCtrl",
+	"id" : 29,
+	"key" : "/system/flew",
+	"level" : 2,
+	"name" : "流程实列",
+	"parent" : 1,
+	"urlCtrl" : "webappJs/sys/flewCtrl",
+	"urlHtml" : "../ftl/sys/flew.html",
+	"urlKey" : "flew",
+	"$$hashKey" : "object:15"
+}, {
+	"code" : "code3006",
+	"ctrlKey" : "ysAnalysisCtrl",
+	"id" : 51,
+	"key" : "/analysis/ysAnalysisCtrl",
+	"level" : 2,
+	"name" : "楼盘优势分析（锐理）",
+	"parent" : 5,
+	"urlCtrl" : "webappJs/analysis/ysAnalysisCtrl",
+	"urlHtml" : "../ftl/analysis/ysA.html",
+	"urlKey" : "ysAnalysis",
+	"$$hashKey" : "object:16"
+}, {
+	"code" : "code3007",
+	"ctrlKey" : "priceAnalysisCtrl",
+	"id" : 52,
+	"key" : "/analysis/priceAnalysisCtrl",
+	"level" : 2,
+	"name" : "区域价格分析（锐理）",
+	"parent" : 5,
+	"urlCtrl" : "webappJs/analysis/priceAnalysisCtrl",
+	"urlHtml" : "../ftl/analysis/priceA.html",
+	"urlKey" : "priceAnalysis",
+	"$$hashKey" : "object:17"
+}, {
+	"code" : "",
+	"ctrlKey" : "",
+	"id" : 221,
+	"key" : "",
+	"level" : 2,
+	"name" : "来呀来呀来呀",
+	"parent" : 2,
+	"urlCtrl" : "",
+	"urlHtml" : "",
+	"urlKey" : "",
+	"$$hashKey" : "object:18"
+}, {
+	"code" : "",
+	"ctrlKey" : "",
+	"id" : 222,
+	"key" : "",
+	"level" : 2,
+	"name" : "来呀来呀来呀2",
+	"parent" : 2,
+	"urlCtrl" : "",
+	"urlHtml" : "",
+	"urlKey" : "",
+	"$$hashKey" : "object:19"
+} ]
